dd5adfd835f611de18a20608ce18946218dd535b
kolen <incredible.angst@gmail.com>
2014-06-29 15:25:12 +0400

    Changed to lambda-based syntax of receive

--- src/main/java/crawler/LinkRegistry.java
+++ src/main/java/crawler/LinkRegistry.java
@@ -1,8 +1,10 @@
 package crawler;
 
+import akka.actor.AbstractActor;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.japi.pf.ReceiveBuilder;
 import crawler.messages.DumpLinks;
 import crawler.messages.FoundLink;
 import crawler.messages.LinksList;
@@ -14,21 +16,21 @@ import java.util.LinkedList;
 /**
  *
  */
-public class LinkRegistry extends UntypedActor {
+public class LinkRegistry extends AbstractActor {
     LoggingAdapter log = Logging.getLogger(getContext().system(), this);
     HashSet<URI> links = new HashSet<>();
 
-    @Override
-    public void onReceive(Object message) throws Exception {
-        if (message instanceof FoundLink) {
-            log.debug("Received FoundLink: "+(FoundLink)message);
-            links.add(((FoundLink) message).getUri());
-        } else if (message instanceof DumpLinks) {
+    public LinkRegistry() {
+        receive(ReceiveBuilder
+        .match(FoundLink.class, msg -> {
+            log.debug("Received FoundLink: "+ msg);
+            links.add(msg.getUri());
+        })
+        .match(DumpLinks.class, msg -> {
             LinkedList<URI> links_list = new LinkedList<>();
             links_list.addAll(links);
-            getSender().tell(new LinksList(links_list), getSelf());
-        } else {
-            unhandled(message);
-        }
+            sender().tell(new LinksList(links_list), self());
+        })
+        .matchAny(this::unhandled).build());
     }
 }
