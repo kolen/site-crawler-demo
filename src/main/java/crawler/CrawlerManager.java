ae80cf68a64d0bac68c88ebcce14a6eea8da9c12
kolen <incredible.angst@gmail.com>
2014-06-30 01:26:15 +0400

    Wait before link dump

--- src/main/java/crawler/CrawlerManager.java
+++ src/main/java/crawler/CrawlerManager.java
@@ -3,14 +3,17 @@ package crawler;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
-import akka.dispatch.OnSuccess;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
 import akka.util.Timeout;
-import crawler.messages.*;
+import crawler.messages.AddDomain;
+import crawler.messages.DomainFinished;
+import crawler.messages.DumpLinks;
+import crawler.messages.StartCrawl;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
 
 import java.net.URI;
 import java.util.HashMap;
@@ -24,6 +27,8 @@ import static akka.pattern.Patterns.pipe;
  *
  */
 public class CrawlerManager extends AbstractActor {
+    public static final FiniteDuration LINK_EXTRACTOR_WAIT_DELAY = Duration.create(10, TimeUnit.SECONDS);
+    public static final FiniteDuration DUMP_LINKS_TIMEOUT = Duration.create(1, TimeUnit.MINUTES);
     private final LoggingAdapter log = Logging.getLogger(context().system(), this);
     private HashMap<String, ActorRef> domainCrawlers = new HashMap<>();
     private HashSet<ActorRef> workingCrawlers = new HashSet<>();
@@ -56,17 +61,15 @@ public class CrawlerManager extends AbstractActor {
                 .match(DomainFinished.class, m -> {
                     workingCrawlers.remove(sender());
                     if (workingCrawlers.isEmpty()) {
-                        log.info("All domains finished, telling "+startInitiator);
-                        log.info("r: "+linkRegistry);
-                        final Future<Object> ask = ask(linkRegistry, new DumpLinks(),
-                                new Timeout(Duration.create(1, TimeUnit.MINUTES)));
-                        pipe(ask, context().system().dispatcher()).to(startInitiator);
-//                        ask.onSuccess(new OnSuccess<Object>() {
-//                            @Override
-//                            public void onSuccess(Object o) throws Throwable {
-//                                System.out.println(((LinksList)o).getLinks());
-//                            }
-//                        }, context().system().dispatcher());
+                        log.info("All domains finished, waiting for extractors to finish");
+
+                        context().system().scheduler().scheduleOnce(LINK_EXTRACTOR_WAIT_DELAY,
+                                () -> {
+                                    log.info("Dumping links");
+                                    final Future<Object> ask = ask(linkRegistry, new DumpLinks(),
+                                            new Timeout(DUMP_LINKS_TIMEOUT));
+                                    pipe(ask, context().system().dispatcher()).to(startInitiator);
+                                }, context().system().dispatcher());
                     }
                 })
                 .match(StartCrawl.class, m -> {
